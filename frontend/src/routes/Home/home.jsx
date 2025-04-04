1e21fc15684c805799b28489ab8786c3eaac9e47
Donald Agamah <donald.agamah@ignition633.org>
2025-04-04 12:12:42 +0100

    Add Home component layout and styling with introductory content

--- frontend/src/routes/Home/home.jsx
+++ frontend/src/routes/Home/home.jsx
@@ -1,9 +1,22 @@
-import './home.css'
+import "./home.css";
 
 const Home = () => {
-  return(
-    <div className='home'>Home</div>
-  )
-}
+  return (
+    <div className="home">
+      <div className="left">
+        <h1>ASSISTANT</h1>
+        <h2>AI; Intensify your Creativity and Productivity</h2>
+        <h3>
+          Assistant is a conversational agent that interacts with users to
+          provide them with information, answer their questions, and help them
+          accomplish tasks. It is a computer program that simulates human
+          conversation through voice commands or text chats or both.
+        </h3>
+        <button>Get Started</button>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
