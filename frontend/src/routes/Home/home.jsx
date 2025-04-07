8993c4da2a6dcbbb80fca876d1e74ad35dec7c90
Donald Agamah <donald.agamah@ignition633.org>
2025-04-07 16:08:38 +0100

    Enhance Home and Guest layouts with responsive design adjustments and add chat feature with typing animation

--- frontend/src/routes/Home/home.jsx
+++ frontend/src/routes/Home/home.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import { TypeAnimation } from "react-type-animation";
 import "./home.css";
+import { useState } from "react";
 
 const Home = () => {
+  const [typingStatus, setTypingStatus] = useState("human1")
+
   return (
     <div className="home">
       <img src="/orbital.png" alt="" className="orbital" />
@@ -20,6 +24,47 @@ const Home = () => {
             <div className="bg"></div>
           </div>
           <img src="/bot.png" alt="" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "human2"
+                  ? "/human2.jpeg"
+                  : "bot.png"
+              }
+              alt=""
+              className="chatImg"
+            />
+            <TypeAnimation
+              sequence={[
+                "Dean: Benefits of AI in healthcare?",
+                2000,
+                () => setTypingStatus("bot"),
+                "Assistant: Faster diagnoses, personalized care, automation.",
+                2000,
+                () => setTypingStatus("human2"),
+                "Victoria: Fun fact about space?",
+                2000,
+                () => setTypingStatus("bot"),
+                "Assistant: A day on Venus is longer than its year.",
+                2000,
+                () => setTypingStatus("human1"),
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" className="termImg" />
+        <div className="links">
+          <Link to="/">Terms of Service</Link>
+          <span>|</span>
+          <Link to="/">Privacy Policy</Link>
         </div>
       </div>
     </div>
